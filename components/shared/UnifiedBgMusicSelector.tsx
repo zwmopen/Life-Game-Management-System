08778b4a5e5679675ed2887c5c3849fa59be3ba9
User <user@example.com>
2026-02-25 08:48:22 +0800

    更新网易云音乐图标为指定图片

--- components/shared/UnifiedBgMusicSelector.tsx
+++ components/shared/UnifiedBgMusicSelector.tsx
@@ -8,9 +8,13 @@ import { cardStyles, inputStyles, buttonStyles, getStyleByTheme, getTextMain, ge
 import { GlobalHelpButton } from '../HelpSystem';
 
 const NetEaseCloudMusicIcon = () => (
-  <svg viewBox="0 0 24 24" width="16" height="16" fill="currentColor">
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-2-3.5c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5-.67-1.5-1.5-1.5-1.5.67-1.5 1.5zm4-8c0-.83-.67-1.5-1.5-1.5s-1.5.67-1.5 1.5v4c0 .83.67 1.5 1.5 1.5s1.5-.67 1.5-1.5v-4z"/>
-  </svg>
+  <img 
+    src="https://gd-hbimg.huaban.com/d11163d659c764327f48c723823bd4b39e77d414573c-dJp63m_fw658" 
+    alt="网易云音乐" 
+    width="16" 
+    height="16"
+    className="rounded-full"
+  />
 );
 
 interface Sound {
